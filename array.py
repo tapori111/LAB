2c6dd6794c3818cc74431cfd8d3388bc8b308daa
Suneet <tapori111@yahoo.com>
2024-05-18 20:21:15 +0800

    learning

---
+++ array.py
@@ -0,0 +1,15 @@
+hp = [100,100,100,100]
+
+print(hp)
+
+hp = []
+
+print(hp)
+hp.append(100)
+hp.append(100)
+hp.append(100)
+hp.append(100)
+print(hp)
+
+hp = [100]*100
+print(hp)
--- hello.py
+++ hello.py
@@ -1,3 +1,27 @@
 import sys
 
 print('hello')
+
+#Init Story
+
+madLibsStory = (f"")
+
+#profession = ["","","",""]
+
+#print(f"Need 4 profesions:")
+#for i in range(len(profession)):
+#    profession[i] = input(f"Profession (plural) {i+1} / {len(profession)}: ")
+
+#print(f"{profession[0]}, {profession[1]}, {profession[2]}, {profession[3]}")
+
+print(1==True)
+
+a = "hello name"
+print(a)
+
+print('name' in a)
+
+a='z'
+b='\t'
+print(a)
+print(b)
